f578a67cfba8f7bb8fe9ab2d10d1a82bd71e3422
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-11-21 17:36:56 +0000

    refactor: add types to functions and rename chunks

--- src/ReverseOrRotate.ts
+++ src/ReverseOrRotate.ts
@@ -1,34 +1,38 @@
-export const cubedReducer = (accumulator: number, currentValue: string) =>
-  Math.pow(parseInt(currentValue), 3) + accumulator;
+const cube = (currentValue: string): number => {
+  return Math.pow(parseInt(currentValue), 3);
+};
 
-export const isEvenWhenSumIsCubed = (input: string) => {
+export const cubedReducer = (
+  accumulator: number,
+  currentValue: string,
+): number => cube(currentValue) + accumulator;
+
+export const isEvenWhenSumIsCubed = (input: string): boolean => {
   const digits = input.split('');
   const sum = digits.reduce(cubedReducer, 0);
 
   return sum % 2 === 0;
 };
 
-const getChuncks = (numbers: string, size: number) => {
-  let remainingLength = numbers.length;
+const getChuncks = (input: string, size: number): Array<string> => {
+  let remainingLength = input.length;
   let startingPosition = 0;
-  const substrings = [];
+  const chunks = [];
 
   while (remainingLength >= size) {
-    substrings.push(
-      numbers.substring(startingPosition, startingPosition + size),
-    );
+    chunks.push(input.substring(startingPosition, startingPosition + size));
     startingPosition += size;
     remainingLength -= size;
   }
 
-  return substrings;
+  return chunks;
 };
 
-const reverse = (chunk: string) => {
-  return chunk.split('').reverse().join('');
+const reverse = (input: string): string => {
+  return input.split('').reverse().join('');
 };
 
-const reverseOrRotate = (numbers: string, size: number) => {
+const reverseOrRotate = (numbers: string, size: number): string => {
   const chunck = getChuncks(numbers, size);
   let result = '';
 
